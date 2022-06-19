48c0d4bb9280d2949ccaae6c1375b6e9b45472cd
Hendrik Tjabben <hendrik.tjabben@student.hpi.de>
2022-06-19 18:55:43 +0200

    Lists available features in a dropdown menu

--- TrackTable.py
+++ TrackTable.py
@@ -11,12 +11,38 @@ import sys
 from os import listdir
 from os.path import isfile, join, splitext
 
+g_custom_algorithm = "Custom Algorithm"
+
+###################################
+# Write custom spot analysis here #
+###################################
+# The 'analyse_spot()' function is#
+# the entry point for custom spot #
+# analysis algorithm. It gets a   #
+# spot as input and must return   #
+# a string to be shown in the     #
+# result table.                   #
+# The 'analyse_spot()' function   #
+# is automatically invoked when   #
+# the 'Custom Algorithm' feature  #
+# is selected in the dropdown     #
+
+def analyse_spot(spot):
+	#anzahl=spots.getNSpots(True)
+	# TODO Write algorithm to analyse spots here.
+	# Must return a single value as string
+	return "Not implemented"
+
+###################################
+
 class Track:
     
-    def __init__(self, id, spots):
+    def __init__(self, id, trackModel):
         self.id = id
+        self.trackModel = trackModel
+        track_spots = trackModel.trackSpots(id)
         self.spots = []
-        for spot in spots:
+        for spot in track_spots:
         	self.spots.append(spot)
 
     def get_id(self):
@@ -26,14 +52,23 @@ class Track:
         if len(self.spots) <= id:
             return None
         return self.spots[id]
+    
+    def get_features(self):
+        features = []
+        for spot in self.spots:
+            features.append(spot.getFeatures().keys())
+        return flat_map_get_common(features)
 
 class TrackTable:
 
-    def __init__(self, spot_analyzer):
+    def __init__(self, tracks, feature, spot_analyzer):
         self.spot_analyzer = spot_analyzer
         self.tracks = []
+        self.feature = feature
         self.column_names = []
     	self.result_table = ResultsTable()
+        for track in tracks:
+            self.addTrack(track)
 
     def addTrack(self, track):
         self.tracks.append(track)
@@ -66,7 +101,10 @@ class TrackTable:
             if spot is None:
                 self.result_table.addValue(column_name, "")
             else:
-                feature = self.spot_analyzer(spot)
+                if self.feature == g_custom_algorithm:
+                    feature = self.spot_analyzer(spot)
+                else:
+                    feature = str(spot.getFeature(self.feature))
                 self.result_table.addValue(column_name, str(feature))
 
     def to_results_table(self):
@@ -74,6 +112,12 @@ class TrackTable:
         self.__add_columns(rows)
         [self.__add_row(row) for row in rows[1:]]
     	return self.result_table
+
+def flat_map_get_common(list_of_lists):
+    result = set(list_of_lists[0])
+    for l in list_of_lists[1:]:
+        result.intersection_update(l)
+    return result
 
 def isXMLFile(file):
     """
@@ -83,12 +127,6 @@ def isXMLFile(file):
         return False
     filename, extension = splitext(file)
     return extension == ".xml"
-
-def analyse_spot(spot):
-	#anzahl=spots.getNSpots(True)
-	# TODO Write algorithm to analyse spots here.
-	# Must return a single value as string
-	return str(spot.getFeature("MEAN_INTENSITY01"))
 
 def get_xmls_to_analyze(path):
     path_file = File(path)
@@ -105,47 +143,79 @@ def get_xmls_to_analyze(path):
             gui.addMessage("Selected file is not a xml file")
             gui.showDialog()
 
-def create_table_for(trackModel):
-	trackTable = TrackTable(analyse_spot)
-	track_ids = trackModel.trackIDs(True)
-	for id in track_ids:
-		track = Track(id, trackModel.trackSpots(id))
-		trackTable.addTrack(track)
+def create_table_for(tracks, feature):
+	trackTable = TrackTable(tracks, feature, analyse_spot)
 	return trackTable.to_results_table()
 
-# Create a GUI window and add required fields
-gui = GenericDialogPlus("TrackTable to CSV")
-gui.addDirectoryOrFileField("Select Directory of XML File", "Select Directory", 16)
-gui.addCheckbox("View Table", True)
-gui.addCheckbox("Auto Save", False)
-gui.addStringField("Add extension to files when saving", "", 16)
-
-# Create logger to output things
-logger = Logger.IJ_LOGGER
-
-gui.showDialog()
-
-if gui.wasOKed():
-    checkboxes = gui.getCheckboxes()
-    path = gui.getNextString()
-    output_extension = gui.getNextString()
-    should_show = checkboxes[0].state
-    should_save = checkboxes[1].state
-    xml_files = get_xmls_to_analyze(path)
+def get_tracks_for_files(xml_files):
+    result = {}
     for xml_file in xml_files:
         if xml_file is None:
             continue
+        result[xml_file] = []
         file = File(xml_file)
         reader = TmXmlReader(file)
         if not reader.isReadingOk():
             continue
         model = reader.getModel()
         trackModel = model.getTrackModel()
-        table = create_table_for(trackModel)
-        filename, extension = splitext(file.getAbsolutePath())
-        if should_show:
-            table.show("Tracks of file " + filename)
-        if should_save:
-            output_file = filename + output_extension + ".csv"
-            table.save(output_file)
-    
+        track_ids = trackModel.trackIDs(True)
+        for id in track_ids:
+            track = Track(id, trackModel)
+            result[xml_file].append(track)
+    return result
+
+def find_mutual_features(tracks_for_files):
+    features_across_files = []
+    for xml_file in tracks_for_files:
+        features_in_file = []
+        for track in tracks_for_files.get(xml_file):
+            features_in_file.append(track.get_features())
+        common_features_in_file = flat_map_get_common(features_in_file)
+        features_across_files.append(common_features_in_file)
+    commons_set = flat_map_get_common(features_across_files)
+    commons_list = []
+    for elem in commons_set:
+        commons_list.append(elem)
+    return commons_list
+
+# Create logger to output things
+logger = Logger.IJ_LOGGER
+
+chooser = GenericDialogPlus("Please select File or Directory containing XMLs")
+chooser.addDirectoryOrFileField("Select Directory or Single XML File", "Select Directory or File", 16)
+chooser.showDialog()
+
+if chooser.wasOKed():
+
+    path = chooser.getNextString()
+    xml_files = get_xmls_to_analyze(path)
+    tracks_for_files = get_tracks_for_files(xml_files)
+    available_features = find_mutual_features(tracks_for_files)
+    available_features.append(g_custom_algorithm)
+
+    # Create a GUI window and add required fields
+    gui = GenericDialogPlus("TrackTable to CSV")
+    gui.addCheckbox("View Table", True)
+    gui.addCheckbox("Auto Save", False)
+    gui.addStringField("Add extension to files when saving", "", 16)
+    gui.addChoice("Choose a feature to extract.", available_features, available_features[0])
+
+    gui.showDialog()
+
+    if gui.wasOKed():
+        checkboxes = gui.getCheckboxes()
+        output_extension = gui.getNextString()
+        feature = str(gui.getNextChoice())
+        should_show = checkboxes[0].state
+        should_save = checkboxes[1].state
+        for xml_file in tracks_for_files:
+            tracks = tracks_for_files[xml_file]
+            table = create_table_for(tracks, feature)
+            file = File(xml_file)
+            filename, extension = splitext(file.getAbsolutePath())
+            if should_show:
+                table.show("Tracks of file " + filename)
+            if should_save:
+                output_file = filename + output_extension + ".csv"
+                table.save(output_file)
